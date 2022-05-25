f24740fde7d570b21cc5c7b1e850b5445a823011
John Doe <johndoe@example.com>
2022-05-25 17:04:37 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit 
+          {' '}
+          <code>src/App.js</code> and save to reload.
         </p>
+        // eslint-disable-next-line react/react-in-jsx-scope
+        // eslint-disable-next-line react/react-in-jsx-scope
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,18 @@
-const reportWebVitals = onPerfEntry => {
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable arrow-parens */
+function reportWebVitals(onPerfEntry) {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals').then(({
+        getCLS, getFID, getFCP, getLCP, getTTFB
+      }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      });
   }
-};
+}
 
 export default reportWebVitals;
